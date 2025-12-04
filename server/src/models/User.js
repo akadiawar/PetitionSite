d8f95be30e8985c4560f2a21b908a03436e156a3
김하민 <130683936+akadiawar@users.noreply.github.com>
2025-12-04 21:36:39 +0900

    User 모델 pre save 미들웨어 오류 수정

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -23,10 +23,9 @@ const userSchema = new mongoose.Schema({
 });
 
 // 비밀번호 해싱
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
